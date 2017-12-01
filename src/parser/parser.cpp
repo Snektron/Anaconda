de66ba0ff7f06da7ff21bce29182a1da80e26daa
RobinVoetter <voetterrb1a@gmail.com>
2017-12-01 14:14:36 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,5 +1,41 @@
 #include "parser/parser.h"
 
-Parser::Parser(std::istream &input):
-	input_(input) {
+Parser::Parser(const char *source):
+	source_(source), current_(0) {}
+
+Node* Parser::parse() {
+	return file();
 }
+
+int Parser::peek()
+{
+	return source_[current_];
+}
+
+void Parser::consume()
+{
+	current_++;
+}
+
+bool Parser::expect(int c) {
+	if (peek() == c) {
+		consume();
+		return true;
+	}
+
+	return false;
+}
+
+size_t Parser::save() {
+	return current_;
+}
+
+void Parser::restore(size_t pos) {
+	current_ = pos;
+}
+
+Node* Parser::file() {
+	return nullptr;
+}
+
+
